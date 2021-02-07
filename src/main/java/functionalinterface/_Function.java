77fc85b08c359f4d167acff2acc734c6e00aaa00
Ch Faizan Mustansar <chaudhry.mustansar@iem.fraunhofer.de>
2021-02-07 22:30:04 +0100

    AndThen examples: This piece of code showing the chaining of Function<T,R> without AndThen() usage. See it is quite filthy at line number 15.    (Function<T,R> exercise)

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -7,14 +7,21 @@ public class _Function {
         int increment = increment(0);
         System.out.println(increment);
 
-
         int increment2 = incrementByOneFunction.apply(0);
         System.out.println(increment2);
+
+        //FM: AndThen() example.
+        int multiply = multiplyByTenFunction.apply(incrementByOneFunction.apply(1)); //20
+        System.out.println(multiplyByTenFunction.apply(incrementByOneFunction.apply(1)));
 
     }
 
     static Function<Integer, Integer> incrementByOneFunction =
             number -> number+=1;
+
+    static Function<Integer, Integer> multiplyByTenFunction =
+            number -> number*=10;
+
 
 
     //Rather than this, please use Function(T,R)
