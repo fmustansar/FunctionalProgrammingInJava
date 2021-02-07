0fc1635073163de54e9f26d103de7c3b4070d008
Ch Faizan Mustansar <chaudhry.mustansar@iem.fraunhofer.de>
2021-02-07 22:14:23 +0100

    Using actual Function<T,R>  (Function<T,R> exercise)

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -1,13 +1,26 @@
 package functionalinterface;
 
+import java.util.function.Function;
+
 public class _Function {
     public static void main(String[] args) {
         int increment = increment(0);
         System.out.println(increment);
 
+
+        int increment2 = incrementByOneFunction.apply(0);
+        System.out.println(increment2);
+
     }
 
+    static Function<Integer, Integer> incrementByOneFunction =
+            number -> number+=1;
+
+
+    //Rather than this, please use Function(T,R)
     static int increment(int number) {
         return number += 1;
     }
+
+
 }
