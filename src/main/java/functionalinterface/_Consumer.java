d73148af29f301e9e2fb78f69cdfb9164c15b602
Ch Faizan Mustansar <chaudhry.mustansar@iem.fraunhofer.de>
2021-02-08 01:47:07 +0100

    (Example:BiConsumer<T>)  Added a new Product class and asked the Customer to buy  aproduct and greets him with what he has just bought. This is BiConsumer

--- src/main/java/functionalinterface/_Consumer.java
+++ src/main/java/functionalinterface/_Consumer.java
@@ -1,17 +1,29 @@
 package functionalinterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
     public static void main(String[] args) {
         //FM: The non functional way, normal java function
-        greetCustomer(new Customer("FAIZAN", "+49-312-4133770"));
+        Customer faizan = new Customer("FAIZAN", "+49-312-4133770");
+        Product sonyV41D = new Product("SonySoundSystem", 50000);
+
+        greetCustomer(faizan);
         //FM: By using the Consumer functional interface
-        greetCustomerConsumer.accept(new Customer("FAIZAN", "+49-312-4133770"));
+        greetCustomerConsumer.accept(faizan);
+
+        //FM: By using the BiConsumer functional interface
+        buyProductByCustomerBiConsumer.accept(faizan, sonyV41D);
     }
 
     //FM: By using the Consumer functional interface
     static Consumer<Customer> greetCustomerConsumer = c -> System.out.println("Hello "+ c.customerName + ", thanks for registering ->"+ c.customerPhoneNumber);
+
+    //FM: By using the BiConsumer functional interface
+    static BiConsumer<Customer, Product> buyProductByCustomerBiConsumer =
+            (c, p) -> System.out.println("Hello "+ c.customerName + ", thanks for buying ->"+ p.productName+" in "+p.productPrice+"/- PKR");
+
 
     //FM: The non functional way, normal java function
     static void greetCustomer(Customer c) {
@@ -26,6 +38,16 @@ public class _Consumer {
         Customer(String customerName, String customerPhoneNumber) {
             this.customerName = customerName;
             this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+    //FM: Same for both functional and non functional way of programming
+    static class Product {
+        private final String productName;
+        private final int productPrice;
+
+        Product(String productName, int productPrice) {
+            this.productName = productName;
+            this.productPrice = productPrice;
         }
     }
 }
