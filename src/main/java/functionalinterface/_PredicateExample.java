80d5a3d1c71e1aa72556aa4d3908242dcb065ccf
Ch Faizan Mustansar <chaudhry.mustansar@iem.fraunhofer.de>
2021-02-08 02:20:36 +0100

    Predicate<T> Example:   Predicate chaining has been added with the help of add() function

--- src/main/java/functionalinterface/_PredicateExample.java
+++ src/main/java/functionalinterface/_PredicateExample.java
@@ -4,19 +4,28 @@ import java.util.function.Predicate;
 
 public class _PredicateExample {
     public static void main(String[] args) {
+        System.out.println("-------------- OLDER Way ---------------------");
         System.out.println(isPhoneNumberValid("+49-3123-4133770"));
         System.out.println(isPhoneNumberValid("+49-3123-41337700"));
         System.out.println(isPhoneNumberValid("+92-3123-4133770"));
-        System.out.println("----------------------------------------------");
+
+        System.out.println("------------------ Predicates Way ---------------------");
         System.out.println(isPhoneNumberValidPredicate.test("+49-3123-4133770"));
         System.out.println(isPhoneNumberValidPredicate.test("+49-3123-41337700"));
         System.out.println(isPhoneNumberValidPredicate.test("+92-3123-4133770"));
 
+        System.out.println("------------------ Predicates Chaining Way ---------------------");
+        System.out.println(isPhoneNumberValidPredicate.and(isPhoneNumberAGermanPhNumber).test("+49-3123-4133770"));
+        System.out.println(isPhoneNumberValidPredicate.and(isPhoneNumberAGermanPhNumber).test("+49-3123-41337700"));
+        System.out.println(isPhoneNumberValidPredicate.and(isPhoneNumberAGermanPhNumber).test("+92-3123-4133770"));
+
+
     }
 
-    static Predicate<String> isPhoneNumberValidPredicate = phNumber ->phNumber.startsWith("+49") && phNumber.length() == 16;
+    static Predicate<String> isPhoneNumberValidPredicate = phNumber ->phNumber.startsWith("+") && phNumber.length() == 16;
+    static Predicate<String> isPhoneNumberAGermanPhNumber = phNumber ->phNumber.startsWith("+49");
 
     static boolean isPhoneNumberValid(String phNumber) {
-        return phNumber.startsWith("+49") && phNumber.length() == 16;
+        return phNumber.startsWith("+") && phNumber.length() == 16;
     }
 }
