f10e243d0335cbc624543779a96d8b33db9331a2
Ch Faizan Mustansar <chaudhry.mustansar@iem.fraunhofer.de>
2021-02-08 02:38:05 +0100

    BiPredicate<T> Example:   BiPredicate example/

--- src/main/java/functionalinterface/_PredicateExample.java
+++ src/main/java/functionalinterface/_PredicateExample.java
@@ -1,5 +1,6 @@
 package functionalinterface;
 
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class _PredicateExample {
@@ -19,6 +20,10 @@ public class _PredicateExample {
         System.out.println(isPhoneNumberValidPredicate.and(isPhoneNumberAGermanPhNumber).test("+49-3123-41337700"));
         System.out.println(isPhoneNumberValidPredicate.and(isPhoneNumberAGermanPhNumber).test("+92-3123-4133770"));
         System.out.println(isPhoneNumberValidPredicate.and(isPhoneNumberAGermanPhNumber.or(isPhoneNumberAPakistaniPhNumber)).test("+92-3123-4133770"));
+        System.out.println("------------------ Bi Predicates Way ---------------------");
+        System.out.println(isPhoneNumberValidForWhichCountry.test("+92-3123-4133770", "+92"));
+        System.out.println(isPhoneNumberValidForWhichCountry.test("+49-3123-4133770", "+49"));
+        System.out.println(isPhoneNumberValidForWhichCountry.test("+49-3123-4133770", "+92"));
 
 
     }
@@ -26,6 +31,11 @@ public class _PredicateExample {
     static Predicate<String> isPhoneNumberValidPredicate = phNumber ->phNumber.startsWith("+") && phNumber.length() == 16;
     static Predicate<String> isPhoneNumberAGermanPhNumber = phNumber ->phNumber.startsWith("+49");
     static Predicate<String> isPhoneNumberAPakistaniPhNumber = phNumber ->phNumber.startsWith("+92");
+
+    //FM: Bi-Predicate example
+    static BiPredicate<String, String> isPhoneNumberValidForWhichCountry =
+            (phNumber, countryPhoneCode) ->  phNumber.startsWith("+") && phNumber.length() == 16 && phNumber.startsWith(countryPhoneCode);
+
     static boolean isPhoneNumberValid(String phNumber) {
         return phNumber.startsWith("+") && phNumber.length() == 16;
     }
