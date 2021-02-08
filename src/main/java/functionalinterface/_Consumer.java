a9456a3476f7c5b8111154a0c81f9f6f14535c6f
Ch Faizan Mustansar <chaudhry.mustansar@iem.fraunhofer.de>
2021-02-08 01:27:24 +0100

    (Example:Consumer<T>)  Both approaches discussed.

--- src/main/java/functionalinterface/_Consumer.java
+++ src/main/java/functionalinterface/_Consumer.java
@@ -1,14 +1,24 @@
 package functionalinterface;
 
+import java.util.function.Consumer;
+
 public class _Consumer {
     public static void main(String[] args) {
+        //FM: The non functional way, normal java function
         greetCustomer(new Customer("FAIZAN", "+49-312-4133770"));
+        //FM: By using the Consumer functional interface
+        greetCustomerConsumer.accept(new Customer("FAIZAN", "+49-312-4133770"));
     }
 
+    //FM: By using the Consumer functional interface
+    static Consumer<Customer> greetCustomerConsumer = c -> System.out.println("Hello "+ c.customerName + ", thanks for registering ->"+ c.customerPhoneNumber);
+
+    //FM: The non functional way, normal java function
     static void greetCustomer(Customer c) {
         System.out.println("Hello "+ c.customerName + ", thanks for registering ->"+ c.customerPhoneNumber);
     }
 
+    //FM: Same for both functional and non functional way of programming
     static class Customer {
         private final String customerName;
         private final String customerPhoneNumber;
