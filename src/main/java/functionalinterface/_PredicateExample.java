69ff7685a2d964335ab2e496dad69e79f2b3d7f8
Ch Faizan Mustansar <chaudhry.mustansar@iem.fraunhofer.de>
2021-02-08 02:05:04 +0100

    Predicate<T> Example:   Now adding Predicates to get the same result.

--- src/main/java/functionalinterface/_PredicateExample.java
+++ src/main/java/functionalinterface/_PredicateExample.java
@@ -7,9 +7,14 @@ public class _PredicateExample {
         System.out.println(isPhoneNumberValid("+49-3123-4133770"));
         System.out.println(isPhoneNumberValid("+49-3123-41337700"));
         System.out.println(isPhoneNumberValid("+92-3123-4133770"));
-
+        System.out.println("----------------------------------------------");
+        System.out.println(isPhoneNumberValidPredicate.test("+49-3123-4133770"));
+        System.out.println(isPhoneNumberValidPredicate.test("+49-3123-41337700"));
+        System.out.println(isPhoneNumberValidPredicate.test("+92-3123-4133770"));
 
     }
+
+    static Predicate<String> isPhoneNumberValidPredicate = phNumber ->phNumber.startsWith("+49") && phNumber.length() == 16;
 
     static boolean isPhoneNumberValid(String phNumber) {
         return phNumber.startsWith("+49") && phNumber.length() == 16;
