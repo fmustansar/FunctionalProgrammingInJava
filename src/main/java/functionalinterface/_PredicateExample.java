a6e2d3cc6a4244bb704611d16b3eb81654a149ee
Ch Faizan Mustansar <chaudhry.mustansar@iem.fraunhofer.de>
2021-02-08 02:26:29 +0100

    Predicate<T> Example:   Predicate chaining has been added with the help of or() function

--- src/main/java/functionalinterface/_PredicateExample.java
+++ src/main/java/functionalinterface/_PredicateExample.java
@@ -18,13 +18,14 @@ public class _PredicateExample {
         System.out.println(isPhoneNumberValidPredicate.and(isPhoneNumberAGermanPhNumber).test("+49-3123-4133770"));
         System.out.println(isPhoneNumberValidPredicate.and(isPhoneNumberAGermanPhNumber).test("+49-3123-41337700"));
         System.out.println(isPhoneNumberValidPredicate.and(isPhoneNumberAGermanPhNumber).test("+92-3123-4133770"));
+        System.out.println(isPhoneNumberValidPredicate.and(isPhoneNumberAGermanPhNumber.or(isPhoneNumberAPakistaniPhNumber)).test("+92-3123-4133770"));
 
 
     }
 
     static Predicate<String> isPhoneNumberValidPredicate = phNumber ->phNumber.startsWith("+") && phNumber.length() == 16;
     static Predicate<String> isPhoneNumberAGermanPhNumber = phNumber ->phNumber.startsWith("+49");
-
+    static Predicate<String> isPhoneNumberAPakistaniPhNumber = phNumber ->phNumber.startsWith("+92");
     static boolean isPhoneNumberValid(String phNumber) {
         return phNumber.startsWith("+") && phNumber.length() == 16;
     }
